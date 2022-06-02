302246de9a9ff7d51fa0ca8a696c09f580e87bd6
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2022-06-02 10:30:15 +0000

    Done!!!!!

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,6 +1,33 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
 
-def f(*args,**kwags):
-  print(args,*args,kwags,)#**kwags)
-f(1,2,3,a=10,b=20)
+#def f(*args,**kwags):
+  #print(args,*args,kwags,)#**kwags)
+#f(1,2,3,a=10,b=20)
+import socket
+import time
+
+HOST = 'data.pr4e.org'
+PORT = 80
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((HOST, PORT))
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
+count = 0
+picture = b""
+
+while True:
+    data = mysock.recv(5120)
+    if len(data) < 1: break
+    #time.sleep(0.25)
+    count = count + len(data)
+    print(len(data), count)
+    picture = picture + data
+
+mysock.close()
+
+pos = picture.find(b"\r\n\r\n")
+print('Header length', pos)
+print(picture[:pos].decode())
+(
+fhand.write(picture)
+fhand.close()
