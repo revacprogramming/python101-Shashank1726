8eee049389b247c53db2d404c25a1e67ae65d4af
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2022-06-08 13:45:22 +0000

    Doin'

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,9 +1,9 @@
 # Network Programming 
-# https://www.py4e.com/lessons/network
+# https://www.py4e.com/lessons/network 
  
 #def f(*args,**kwags):
   #print(args,*args,kwags,)#**kwags)
-#f(1,2,3,a=10,b=20)
+#f(1,2,3,a=10,b=20)  
 import socket
 import time
 
