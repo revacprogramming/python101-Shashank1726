49d03fbaa73da4189fc583c6d4d783d8ee01509b
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2022-06-13 05:39:18 +0000

    Doinnnn

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,7 +1,7 @@
 # Network Programming 
 # https://www.py4e.com/lessons/network 
  
-#def f(*args,**kwags):         
+#def f(*args,**kwags):          
   #print(args,*args,kwags,)#**kwags)
 #f(1,2,3,a=10,b=20)  
 import socket
